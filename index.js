525631d5e5dd4b09687b57c1c1ccaf132f8562e1
May <maymchen1@gmail.com>
2022-01-20 21:09:38 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function recievesAFunction() {
+    runFiveMiles();
+    liftWeights();
+
+{
+function returnsANamedFunction() {
+    return function ()
+ {
+     console.log("before all")
+ }}
+function returnsAnAnonymousFunction() {
+    return function ()
+{console.log ("before all")}
+}
